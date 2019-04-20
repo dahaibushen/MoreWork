6512d59cddcb5e0e72a22f2cfd1f667023ba86b1
hu <>
2019-04-20 09:29:56 +0800

    添加model库

---
+++ HYYModel/HYYModel/HYYModel.h
@@ -0,0 +1,14 @@
+//
+//  HYYModel.h
+//  HYYModel
+//
+//  Created by hu on 2019/4/20.
+//  Copyright © 2019 huyiyong. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <JSONModel.h>
+
+@interface HYYModel : NSObject
+
+@end
---
+++ HYYModel/HYYModel/HYYModel.m
@@ -0,0 +1,13 @@
+//
+//  HYYModel.m
+//  HYYModel
+//
+//  Created by hu on 2019/4/20.
+//  Copyright © 2019 huyiyong. All rights reserved.
+//
+
+#import "HYYModel.h"
+
+@implementation HYYModel
+
+@end
